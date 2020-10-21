91102545f8cfaadf1e6483beadcd2b19a0fed51f
Akshay.R.R <65683151+akshay1027@users.noreply.github.com>
2020-10-21 16:23:46 +0530

    added new components(country)

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,15 @@ import styles from '../styles/Home.module.css'
 import Head from "next/head"
 import Link from "next/link"
 import Layout from "../components/layout"
+
 export default function Home() {
   return (
     <Layout>
         <Head>
             <title>News-webapp</title>
         </Head>
-     <h1>Hello Coder</h1>
-     <Link href="/about">
-            <a>about</a>
-      </Link>
+    
+      
       </Layout>
   )
 }
